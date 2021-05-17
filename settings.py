de1aa37baec0f81fb572b9b48b09eeb187958c8f
ezhov93 <30632143+ezhov93@users.noreply.github.com>
2021-05-17 13:31:20 +0300

    Update settings.py
    change sheet link

--- settings.py
+++ settings.py
@@ -1,2 +1,3 @@
 CREDENTIALS_FILE = 'worktrackersheers-df7412d385fb.json'
-SPREADSHEET_ID = '11PIScIi6t7JFvTHYRrglmz_DLxZzBxqqostGiUEx_FI'
+# SPREADSHEET_ID = '11PIScIi6t7JFvTHYRrglmz_DLxZzBxqqostGiUEx_FI'
+SPREADSHEET_ID = '1qUG3rWsJ35loub_zWiE7XrcpL-f-vip_ywXQ42LIoTg'
